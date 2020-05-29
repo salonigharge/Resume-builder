50b3c62203aaa066078fb444016db0d27acc95ee
saloni <salonigharge035@gmail.com>
2020-05-29 12:36:17 +0530

    logout added

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php // line added to turn on color syntax highlight
+
+session_start();
+unset($_SESSION['name']);
+unset($_SESSION['user_id']);
+header('Location: index.php');
+  
+?>
+<html>
+<head>
+	<title>GHARGE SALONI ANNASAHEB</title>
+</head>
+</html>	
